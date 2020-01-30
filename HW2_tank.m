2b2bd31a72a1dcb9ff5036f3dc83116927144df4
jmbayley <53278578+jmbayley@users.noreply.github.com>
2020-01-30 13:09:49 -0800

    Add files via upload

---
+++ HW2_tank.m
@@ -0,0 +1,38 @@
+function HW2_tank %Main/Top-level function
+
+
+
+end
+
+function diam = D(h) %Diameter function as sine function WRT height
+
+
+
+end
+
+function height_dt = rate(t,h) %Rate of change for height of water
+
+
+
+end
+
+function depth_at_time = depth(t) %Depth of water @ time t using ODE45
+
+
+
+
+end
+
+function height_difference = height_diff(t) %Height difference between height @ time t and desired depth
+
+
+
+
+end
+
+function analytic_time = analytic(d,h) %General time to drain a tank of diameter d
+
+
+
+
+end
