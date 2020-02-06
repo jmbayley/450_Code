1f379c0fcd84ed3051f0e93e00fdc5cf6968adae
jmbayley <53278578+jmbayley@users.noreply.github.com>
2020-02-06 15:09:07 -0800

    Add files via upload

---
+++ Single_Driver_Model.m
@@ -0,0 +1,18 @@
+function Single_Driver_Model
+global v0 Thead a b delta s0 lveh Xblock
+
+% set model parameters
+v0 = 28; % m/s (28 m/s ~ 60 mph)
+Thead = .5; % s
+a = 0.5; % m/s^2
+b = 5; % m/s^2
+delta = 4; % exponent
+s0 = 2; % m gap
+lveh = 5; % m -- vehicle length
+Xblock = 3000; % blockage
+
+
+end
+
+
+%URL Link: https://canvas.uw.edu/courses/1364268/assignments/5158807
