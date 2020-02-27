136668ccff41343b01d732c179230f77eb488e68
jmbayley <53278578+jmbayley@users.noreply.github.com>
2020-02-27 13:58:47 -0800

    Add files via upload

--- fourier_example.m
+++ fourier_example.m
@@ -4,10 +4,10 @@ function fourier_example
 % First, create a digital time series with a known composition of sine and cosine functions
 
 Fs = 500; % Hz  sampling frequency: samples per second
-dT = ?; % sec   time between samples from Fs
+dt = 1/Fs; % sec   time between samples from Fs
 nt  = 2000; %     number of samples in record
-T = ?; % Length of record, fundamental period
-t=?;  %  time array in seconds (nt points starting at t=0, ending at T, with dT interval)
+T = (nt-1)*dt; % Length of record, fundamental period
+t=0:dt:T;  %  time array in seconds (nt points starting at t=0, ending at T, with dT interval)
 
 freq0 = 1/T; % Hz   fundamental frequency (smallest frequency) 
 
@@ -17,7 +17,7 @@ freq = freq0*(0:nfmax-1); % frequency array
 freqmax = freq(nfmax); % maximum frequency resolved by FFT (Nyquist)
 
 disp('Fundemental period and Nyquist Freq')
-disp([T, freq])
+disp([T, freqmax])
 
 % Create a time series by summing sine and cosine waves
 
@@ -25,10 +25,21 @@ disp([T, freq])
 %  the fundamental frequency freq0. 
 % Use arbitrary multipliers less than about 10.
 %   
-f1=3*f0;
-f2=6*f0;
-f3=2*f0;
-f4=8*f0;
+f1=3*freq0;
+f2=6*freq0;
+f3=12*freq0;
+f4=15*freq0;
+
+% Uncomment to try non-integer multiple of f0
+% $$$ f4=15.2*freq0;
+
+
+% Uncomment to try these, near the nyquist freq
+% $$$ f1=freqmax;
+% $$$ f2=freqmax/2;
+% $$$ f3=freqmax/5;
+% $$$ f4=freqmax/8;
+
 
 disp('Frequencies selected:')
 disp([f1 f2 f3 f4])
@@ -46,7 +57,7 @@ B = 4;
 C = -2;
 D = 6;
 
-f =  ?;   %%% FILL IN WITH JUST ONE LINE
+f =  A*sin(f1*2*pi*t) + B*sin(f2*2*pi*t) + C*cos(f3*2*pi*t) + D*cos(f4*2*pi*t);   %%% FILL IN WITH JUST ONE LINE
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Second take FFT of your function
@@ -72,9 +83,10 @@ plot(t,f);
 title('Signal');
 
 subplot(2,1,2)
-plot(freq, a, 'o', freq, b, '*', freq,p,'-')
+plot(freq/freq0, a, 'o', freq/freq0, b, '*', freq/freq0,p,'-')
 legend('Cosine', 'Sine', 'Power', 'Location', 'BestOutside')
+xlabel('Multiple of Fundemental Freq')
 
 title('FFT Fourier Coefficients');
-xmax = max([f1 f2 f3 f4])*1.15;
+xmax = max([f1 f2 f3 f4])/freq0*1.15;
 xlim([0 xmax]);
